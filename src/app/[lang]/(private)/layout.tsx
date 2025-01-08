7de7b0ee4c1663b0aa165baf259c5c3056f6efdd
Mitali Patel <mitali.khundiwala@gmail.com>
2025-01-08 15:41:17 +0400

    index on features/internationalization: c9be860 Merge pull request #23 from jitenderchand/features/orders-page-improvements

--- src/app/[lang]/(private)/layout.tsx
+++ src/app/[lang]/(private)/layout.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/sidebar";
 
 import DropdownMenuComponent from "@/modules/common/components/navigation/drop-down-menu";
+import LocaleSwitcher from "@/modules/common/components/navigation/locale-switcher";
 
 export const metadata = {
   title: "My App",
@@ -25,7 +26,12 @@ const AuthLayout: React.FC<RootLayoutProps> = ({ children }) => {
       <SidebarInset>
         <header className="flex justify-between h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <DropdownMenuComponent />
+          <div className="flex">
+            <div className="flex mr-2">
+              <LocaleSwitcher />
+            </div>
+            <DropdownMenuComponent />
+          </div>
         </header>
         <main className="flex-1 px-6 bg-gray-100">{children}</main>
       </SidebarInset>
--- src/app/[lang]/(private)/orders/page.tsx
+++ src/app/[lang]/(private)/orders/page.tsx
@@ -1,4 +1,6 @@
+import { getDictionary } from "@/app/actions/dictionary";
 import Orders from "@/modules/orders/orders";
+import { Locale } from "i18n.config";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,10 +14,22 @@ export default async function OrdersPage(props: {
     size?: string;
     statuses?: string;
   }>;
+  params: Promise<{ lang: Locale }>;
 }) {
+  const params = await props.params;
+  const lang = (await params).lang;
+  const intl = await getDictionary(lang);
+  console.log(intl);
   const searchParams = await props.searchParams;
   const currentPage = searchParams?.page ? Number(searchParams.page) : 1;
   const size = searchParams?.size ? Number(searchParams.size) : 10;
   const statuses = searchParams?.statuses ? searchParams.statuses : null;
-  return <Orders page={currentPage} size={size} statuses={statuses} />;
+  return (
+    <Orders
+      page={currentPage}
+      size={size}
+      statuses={statuses}
+      params={params}
+    />
+  );
 }
